1a1474642dd14643671176c80205dada4ddfe160
joseph <kadeska23@gmail.com>
2016-11-23 19:09:00 -0800

    more on shop

--- src/me/PointShop/Shop.java
+++ src/me/PointShop/Shop.java
@@ -2,6 +2,7 @@ package me.PointShop;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -11,8 +12,21 @@ import org.bukkit.inventory.ItemStack;
 
 public class Shop {
 
+
 	public void onEnable() {
-		Main.getPlugin().saveDefaultConfig();
+		Main.getPlugin().saveConfig();
+	}
+	
+    FileConfiguration config = Main.getPlugin().getConfig();
+	
+	public  void getConfig() {
+		
+		
+		
+		int P1name = config.getInt("P1name:");
+		int P2name = config.getInt("P2name:");
+		int P3name = config.getInt("P3name:");
+		
 	}
 
 	public void onCommand(PlayerCommandPreprocessEvent event) {
@@ -26,7 +40,7 @@ public class Shop {
 				event.setCancelled(true);
 			}
 		}
-	}
+	} 
 
 	public static Inventory myInv = Bukkit.createInventory(null, 18, "GUI Shop Page01");
 
@@ -44,12 +58,7 @@ public class Shop {
 		myInv.setItem(9, new ItemStack(Material.PAPER, 1)); // go to next page
 
 	}
-
-	public void InventoryClick(InventoryClickEvent event) {
-		Player player = (Player) event.getWhoClicked();
-		ItemStack clicked = event.getCurrentItem();
-		Inventory inventory = event.getInventory();
-	}
+	
 
 	@EventHandler
 	public void onInventoryClick(InventoryClickEvent event) {
@@ -59,7 +68,8 @@ public class Shop {
 
 		if (inventory.getName().equals(myInv.getName())) {
 			if (Clicked.getType() == Material.IRON_BOOTS) {
-				if (Managers.getScore(player) == 50) {
+				if (Managers.getScore(player) >= 50) {
+					Managers.removeScore(player, 50);
 					player.closeInventory();
 					player.getInventory().addItem(new ItemStack(Material.IRON_BOOTS, 1));
 					player.openInventory(myInv);
@@ -70,19 +80,113 @@ public class Shop {
 					event.setCancelled(true);
 
 			}
-			if (Clicked.getType() == Material.WOOD) {
-				event.setCancelled(true);
-				player.closeInventory();
-				player.getInventory().addItem(new ItemStack(Material.WOOD, 1));
-				player.openInventory(myInv);
+			if (Clicked.getType() == Material.IRON_LEGGINGS) {
+				if (Managers.getScore(player) >= 50) {
+					Managers.removeScore(player, 50);
+					player.closeInventory();
+					player.getInventory().addItem(new ItemStack(Material.IRON_LEGGINGS, 1));
+					player.openInventory(myInv);
+				} else
+					// player.spigot().sendMessage(new TextComponent( "You do
+					// not have enough points! you need 50 points to buy this
+					// item!"));
+					event.setCancelled(true);
 
 			}
-
-		}
+			if (Clicked.getType() == Material.IRON_HELMET) {
+				if (Managers.getScore(player) >= 50) {
+					Managers.removeScore(player, 50);
+					player.closeInventory();
+					player.getInventory().addItem(new ItemStack(Material.IRON_HELMET, 1));
+					player.openInventory(myInv);
+				} else
+					// player.spigot().sendMessage(new TextComponent( "You do
+					// not have enough points! you need 50 points to buy this
+					// item!"));
+					event.setCancelled(true);
+			}
+			if (Clicked.getType() == Material.IRON_SWORD) {
+				if (Managers.getScore(player) >= 70) {
+					Managers.removeScore(player, 70);
+					player.closeInventory();
+					player.getInventory().addItem(new ItemStack(Material.IRON_SWORD, 1));
+					player.openInventory(myInv);
+				} else
+					// player.spigot().sendMessage(new TextComponent( "You do
+					// not have enough points! you need 50 points to buy this
+					// item!"));
+					event.setCancelled(true);
+			}
+			if (Clicked.getType() == Material.IRON_AXE) {
+				if (Managers.getScore(player) >= 70) {
+					Managers.removeScore(player, 70);
+					player.closeInventory();
+					player.getInventory().addItem(new ItemStack(Material.IRON_AXE, 1));
+					player.openInventory(myInv);
+				} else
+					// player.spigot().sendMessage(new TextComponent( "You do
+					// not have enough points! you need 50 points to buy this
+					// item!"));
+					event.setCancelled(true);
+			}
+			if (Clicked.getType() == Material.IRON_PICKAXE) {
+				if (Managers.getScore(player) >= 70) {
+					Managers.removeScore(player, 70);
+					player.closeInventory();
+					player.getInventory().addItem(new ItemStack(Material.IRON_PICKAXE, 1));
+					player.openInventory(myInv);
+				} else
+					// player.spigot().sendMessage(new TextComponent( "You do
+					// not have enough points! you need 50 points to buy this
+					// item!"));
+					event.setCancelled(true);
+			}
+			if (Clicked.getType() == Material.IRON_SPADE) {
+				if (Managers.getScore(player) >= 70) {
+					Managers.removeScore(player, 70);
+					player.closeInventory();
+					player.getInventory().addItem(new ItemStack(Material.IRON_SPADE, 1));
+					player.openInventory(myInv);
+				} else
+					// player.spigot().sendMessage(new TextComponent( "You do
+					// not have enough points! you need 50 points to buy this
+					// item!"));
+					event.setCancelled(true);
+			}
+			if (Clicked.getType() == Material.IRON_HOE) {
+				if (Managers.getScore(player) >= 70) {
+					Managers.removeScore(player, 70);
+					player.closeInventory();
+					player.getInventory().addItem(new ItemStack(Material.IRON_HOE, 1));
+					player.openInventory(myInv);
+				} else
+					// player.spigot().sendMessage(new TextComponent( "You do
+					// not have enough points! you need 50 points to buy this
+					// item!"));
+					event.setCancelled(true);
+			}
+			if (Clicked.getType() == Material.PAPER) {
+				if (Managers.getScore(player) >= 70) {
+					Managers.removeScore(player, 70);
+					player.closeInventory();
+					player.getInventory().addItem(new ItemStack(Material.PAPER, 1));
+					player.openInventory(myInv);
+				} else
+					// player.spigot().sendMessage(new TextComponent( "You do
+					// not have enough points! you need 50 points to buy this
+					// item!"));
+					event.setCancelled(true);
+			}
+		} else
+			if (!(inventory.getName().equals(myInv.getName()))) {
+			event.setCancelled(true);
+			player.closeInventory();
+			}
+		
 	}
 
 	public void onDisable() {
-		Main.getPlugin().saveDefaultConfig();
+		Main.getPlugin().saveConfig();
 	}
 
 }
